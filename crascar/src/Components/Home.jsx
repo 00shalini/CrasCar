ff63bfe9bd1aa38d90639dbf270cc82c8d4df182
Shalini Sharma <shalini07061997@gmsail.com>
2021-09-19 20:20:07 +0530

    navigation

--- crascar/src/Components/Home.jsx
+++ crascar/src/Components/Home.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
-    return (
-        <div style={{paddingTop:'100px',zIndex:'-900',position:'absolute'}}>
-            <div>
-            </div>
-        </div>
-    )
+  return (
+    <div style={{ paddingTop: "100px", zIndex: "-900", position: "absolute" }}> 
+      <img src='https://cdn.wallpapersafari.com/25/34/tNEzBb.jpg' style={{marginTop:'-10px',width:'98.88vw'}}/>
+      <div className='demobtn'>VIEW CRASH DETAIL PAGE</div>
+    </div>
+  );
 }
 
-export {Home}
+export { Home };
